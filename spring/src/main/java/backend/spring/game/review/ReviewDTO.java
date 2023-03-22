79f785f3664010ced82d79987e682ed6d68cd1f9
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-03-23 00:16:32 +0100

    🏗️🧪 Konstruktoren bei DTOs erstellt

--- spring/src/main/java/backend/spring/game/review/ReviewDTO.java
+++ spring/src/main/java/backend/spring/game/review/ReviewDTO.java
@@ -18,4 +18,12 @@ public class ReviewDTO {
     private EnduserDTO enduser;
     private double rating;
     private Instant createdAt;
+
+    public ReviewDTO(Review review) {
+        this.id = review.getReviewId ();
+        this.content = review.getContent ();
+        this.enduser = new EnduserDTO (review.getEnduser ());
+        this.rating = review.getRating ();
+        this.createdAt = review.getCreatedAt ();
+    }
 }
--- spring/src/main/java/backend/spring/profile/ProfileDTO.java
+++ spring/src/main/java/backend/spring/profile/ProfileDTO.java
@@ -2,10 +2,16 @@ package backend.spring.profile;
 
 
 import backend.spring.enduser.Enduser;
+import backend.spring.game.comment.CommentDTO;
+import backend.spring.game.review.Review;
+import backend.spring.game.review.ReviewDTO;
 import backend.spring.security.DAO.Role;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 @AllArgsConstructor
@@ -16,6 +22,9 @@ public class ProfileDTO {
     private String email;
     private String image;
     private Role role;
+    private String password;
+    private List<ReviewDTO> reviews;
+    private List<CommentDTO> comments;
 
     public ProfileDTO(Enduser enduser){
         this.enduserId = enduser.getEnduserId();
@@ -23,5 +32,12 @@ public class ProfileDTO {
         this.email = enduser.getEmail();
         this.image = enduser.getImage();
         this.role = enduser.getRole();
+        this.password= enduser.getPassword();
+        this.reviews = enduser.getReviews ().stream()
+                .map(review -> new ReviewDTO (review))
+                .collect(Collectors.toList());
+        this.comments = enduser.getComments().stream()
+                .map(comment -> new CommentDTO(comment))
+                .collect(Collectors.toList());
     }
 }
