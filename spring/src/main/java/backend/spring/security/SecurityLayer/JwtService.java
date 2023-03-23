98f785c14e930a804b0e6021ad1fe6f0396e57e2
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-03-23 22:22:48 +0100

    🧪🏗️ Rolen funktionieren in Spring Security 🥳

--- spring/src/main/java/backend/spring/security/SecurityLayer/JwtService.java
+++ spring/src/main/java/backend/spring/security/SecurityLayer/JwtService.java
@@ -1,11 +1,14 @@
 package backend.spring.security.SecurityLayer;
 
 
+import backend.spring.enduser.Enduser;
+import backend.spring.enduser.EnduserRepository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +18,11 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
+@RequiredArgsConstructor
 @Service
 public class JwtService {
 
+    private final EnduserRepository enduserRepository;
     private static final String SECRET_KEY = "327235753778214125442A472D4B6150645367566B59703373367639792F423F";
 
     public String extractUsername(String token) {
@@ -54,6 +59,10 @@ public class JwtService {
             Map<String,Object> extraClaims,
             UserDetails userDetails
     ){
+
+        Enduser enduser = enduserRepository.findByUsername(userDetails.getUsername())
+                .orElseThrow(() -> new IllegalArgumentException("User not found: " + userDetails.getUsername()));
+        extraClaims.put ("role", enduser.getRole().name());
         return Jwts
                 .builder ()
                 .setClaims (extraClaims)
