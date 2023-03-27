dbd860fdcd8c331d2ac3195563a2254fb6c801c0
Sebastian Tamayo <sebastian-tamayo.pacheco@brightstraining.com>
2023-03-27 10:58:26 +0200

    🆙 Alle Kategorien sind da, muss ich aber noch die Bearbeitung funktion hinzufügen

--- angular/src/app/profile-page/user-comments/user-comments.component.ts
+++ angular/src/app/profile-page/user-comments/user-comments.component.ts
@@ -1,5 +1,10 @@
 import {Component, Input} from '@angular/core';
 import {ProfilDTO} from "../../interfaces/profil-dto";
+import {Comment} from "../../interfaces/comment";
+import {switchMap} from "rxjs";
+import {Game} from "../../interfaces/game";
+import {HttpClient} from "@angular/common/http";
+import {GameService} from "../../services/game.service";
 
 @Component({
   selector: 'app-user-comments',
@@ -9,5 +14,15 @@ import {ProfilDTO} from "../../interfaces/profil-dto";
 export class UserCommentsComponent {
   @Input() profile: ProfilDTO | any;
 
+  constructor(private http:HttpClient,private gameService:GameService) {
+  }
 
-}
+
+   deleteComment(comment: Comment) {
+    this.http.post<number>('http://localhost:8080/api/comments/delete', comment.commentId);
+    };
+  }
+
+
+
+
